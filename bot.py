7bf6edb90f8022911fe333c231ee3b1a81d802fb
Alena <sweetysweetygirl@gmail.com>
2024-10-29 19:33:03 +0300

    Modified bot.py, added ConversationHandler.

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, CallbackQueryHandler,
-                          CommandHandler, ContextTypes, MessageHandler,
-                          filters)
+                          CommandHandler, ContextTypes, ConversationHandler,
+                          MessageHandler, filters)
 
 from constants import (CORRECT_ANSWER, PERSONS, QUIZ_BUTTONS,
                        TRANSLATE_PERSONS, TRANSLATE_QUIZ_TOPICS)
 from credentials import BOT_TOKEN, ChatGPT_TOKEN
 from gpt import ChatGptService
-from util import (Dialog, default_callback_handler, load_message, load_prompt,
-                  send_html, send_image, send_response, send_text,
-                  send_text_buttons, show_main_menu)
+from util import (load_message, load_prompt, send_html, send_image,
+                  send_response, send_text, send_text_buttons, show_main_menu)
+
+MAIN, RANDOM, GPT, TALK, QUIZ = range(5)
+
+chat_gpt: ChatGptService = ChatGptService(ChatGPT_TOKEN)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -17,7 +20,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     Обработчик команды /start.
     Отправляет пользователю приветственное сообщение и главное меню.
     """
-    dialog.mode = 'main'
     text: str = load_message('main')
     await send_response(update, context, 'main', text)
     await show_main_menu(update, context, {
@@ -27,6 +29,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         'talk': 'Поговорить с известной личностью 👤',
         'quiz': 'Поучаствовать в квизе ❓'
     })
+    return MAIN
 
 
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -34,7 +37,6 @@ async def random(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     Обработчик команды /random.
     Отправляет случайный интересный факт пользователю.
     """
-    dialog.mode = 'random'
     prompt: str = load_prompt('random')
     message: str = load_message('random')
     message = await send_response(update, context, 'random', message)
@@ -42,22 +44,25 @@ async def random(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         answer: str = await chat_gpt.send_question(prompt, '')
         await message.edit_text(answer)
-        buttons: dict[str, str] = {'random_fact': 'Еще рандомный факт'}
+        buttons: dict[str, str] = {'random_fact': 'Еще рандомный факт',
+                                   'main_menu': 'Главное меню'}
         await send_text_buttons(
             update, context, 'Хотите еще один факт?', buttons)
 
     except Exception as e:
         await message.edit_text(f'Произошла ошибка: {str(e)}')
+    return RANDOM
 
 
 async def random_fact(
         update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
-    Обработчик для кнопки "Еще рандомный факт".
+    Обработчик для кнопки 'Еще рандомный факт'.
     Вызывает повторное выполнение функции random для получения нового факта.
     """
     await update.callback_query.answer()
     await random(update, context)
+    return RANDOM
 
 
 async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -66,11 +71,11 @@ async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     Настраивает режим бота на общение с ChatGPT и отправляет
     соответствующее сообщение.
     """
-    dialog.mode = 'gpt'
     prompt: str = load_prompt('gpt')
     chat_gpt.set_prompt(prompt)
     message: str = load_message('gpt')
     await send_response(update, context, 'gpt', message)
+    return GPT
 
 
 async def gpt_dialog(
@@ -85,8 +90,12 @@ async def gpt_dialog(
     try:
         answer: str = await chat_gpt.add_message(text)
         await message.edit_text(answer)
+        buttons: dict[str, str] = {'main_menu': 'Главное меню'}
+        await send_text_buttons(
+            update, context, 'Вернутья в главное меню', buttons)
     except Exception as e:
         await message.edit_text(f'Произошла ошибка: {str(e)}')
+    return GPT
 
 
 async def show_persons(
@@ -99,6 +108,7 @@ async def show_persons(
     buttons: dict[str, str] = {name: TRANSLATE_PERSONS[name]
                                for name in PERSONS.keys()}
     await send_text_buttons(update, context, '🔍 Выберите личность:', buttons)
+    return TALK
 
 
 async def select_person(
@@ -110,8 +120,8 @@ async def select_person(
     await update.callback_query.answer()
     person: str = update.callback_query.data
     context.user_data['person'] = person
-    dialog.mode = 'talk'
     await talk_with_person(update, context)
+    return TALK
 
 
 async def talk_with_person(
@@ -133,6 +143,7 @@ async def talk_with_person(
             'Напишите что-нибудь!')
     else:
         await show_persons(update, context)
+    return TALK
 
 
 async def change_person(
@@ -143,6 +154,7 @@ async def change_person(
     """
     await update.callback_query.answer()
     await show_persons(update, context)
+    return TALK
 
 
 async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -156,11 +168,13 @@ async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_message: str = update.message.text
         answer: str = await chat_gpt.add_message(user_message)
         await send_text(update, context, answer)
-        buttons: dict[str, str] = {'change_person': 'Выбрать другую личность'}
+        buttons: dict[str, str] = {'change_person': 'Выбрать другую личность',
+                                   'main_menu': 'Главное меню'}
         await send_text_buttons(
             update, context, 'Выбрать другую личность?', buttons)
     else:
         await show_persons(update, context)
+    return TALK
 
 
 async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -168,9 +182,9 @@ async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     Обработчик команды /quiz.
     Отправляет сообщение с инструкциями и кнопками для выбора темы квиза.
     """
-    dialog.mode = 'quiz'
     await send_response(update, context, 'quiz', load_message('quiz'))
     await send_text_buttons(update, context, 'Выберите тему:', QUIZ_BUTTONS)
+    return QUIZ
 
 
 async def quiz_topic_selected(
@@ -186,6 +200,7 @@ async def quiz_topic_selected(
     context.user_data['correct_answers'] = 0
     answer: str = await chat_gpt.add_message(user_message)
     await send_html(update, context, answer)
+    return QUIZ
 
 
 async def handle_quiz_answer(
@@ -209,10 +224,12 @@ async def handle_quiz_answer(
             update, context,
             f'Правильных ответов по теме {current_topic}: {current_score}')
         buttons: dict[str, str] = {'quiz_more': 'Задать ещё вопрос',
-                                   'change_quiz_topic': 'Сменить тему квиза'}
+                                   'change_quiz_topic': 'Сменить тему квиза',
+                                   'main_menu': 'Главное меню'}
         await send_text_buttons(
             update, context, 'Хотите задать ещё вопрос или сменить тему?',
             buttons)
+    return QUIZ
 
 
 async def change_quiz_topic(
@@ -224,6 +241,7 @@ async def change_quiz_topic(
     await update.callback_query.answer()
     await send_text_buttons(
         update, context, 'Выберите новую тему:', QUIZ_BUTTONS)
+    return QUIZ
 
 
 async def quiz_more(
@@ -236,50 +254,70 @@ async def quiz_more(
     await update.callback_query.answer()
     topic: str = context.user_data.get('quiz_topic')
     if topic:
-        question: str = f"Задай вопрос по теме {topic}."
+        question: str = f'Задай вопрос по теме {topic}.'
         answer: str = await chat_gpt.add_message(question)
         await send_html(update, context, answer)
     else:
         await send_text_buttons(
             update, context, 'Выберите тему:', QUIZ_BUTTONS)
+    return QUIZ
 
 
-async def text_handler(
+async def main_menu(
         update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """
-    Обработчик текстовых сообщений. Определяет режим работы бота
-    и вызывает соответствующий обработчик.
-    """
-    if dialog.mode == 'gpt':
-        await gpt_dialog(update, context)
-    elif dialog.mode == 'talk':
-        await talk(update, context)
-    elif dialog.mode == 'quiz':
-        await handle_quiz_answer(update, context)
-    else:
-        await start(update, context)
+    await update.callback_query.answer()
+    await start(update, context)
+    return MAIN
 
 
-dialog: Dialog = Dialog()
-dialog.mode = None
-chat_gpt: ChatGptService = ChatGptService(ChatGPT_TOKEN)
+conv_handler = ConversationHandler(
+    entry_points=[
+        CommandHandler('start', start),
+        CommandHandler('random', random),
+        CommandHandler('gpt', gpt),
+        CommandHandler('talk', talk),
+        CommandHandler('quiz', quiz)
+    ],
+    states={
+        MAIN: [
+            CallbackQueryHandler(random_fact, pattern='^random_fact$'),
+            CallbackQueryHandler(main_menu, pattern='^main_menu$'),
+        ],
+        RANDOM: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, random),
+            CallbackQueryHandler(random_fact, pattern='^random_fact$'),
+            CallbackQueryHandler(main_menu, pattern='^main_menu$'),
+
+        ],
+        GPT: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_dialog),
+            CallbackQueryHandler(main_menu, pattern='^main_menu$'),
+        ],
+        TALK: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, talk),
+            CallbackQueryHandler(
+                select_person,
+                pattern='^(Cobain|Hawking|Nietzsche|Queen|Tolkien)$'),
+            CallbackQueryHandler(change_person, pattern='^change_person$'),
+            CallbackQueryHandler(main_menu, pattern='^main_menu$')
+        ],
+        QUIZ: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND, handle_quiz_answer),
+            CallbackQueryHandler(
+                quiz_topic_selected,
+                pattern='^(quiz_prog|quiz_math|quiz_biology)$'),
+            CallbackQueryHandler(quiz_more, pattern='^quiz_more$'),
+            CallbackQueryHandler(
+                change_quiz_topic, pattern='^change_quiz_topic$'),
+            CallbackQueryHandler(main_menu, pattern='^main_menu$'),]
+    },
+    fallbacks=[
+        CommandHandler('start', start)
+    ],
+    allow_reentry=True
+)
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-app.add_handler(CommandHandler('start', start))
-app.add_handler(CommandHandler('random', random))
-app.add_handler(CommandHandler('gpt', gpt))
-app.add_handler(CommandHandler('talk', talk))
-app.add_handler(CommandHandler('quiz', quiz))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
-app.add_handler(CallbackQueryHandler(random_fact, pattern='^random_fact$'))
-app.add_handler(CallbackQueryHandler(
-    select_person, pattern='^(Cobain|Hawking|Nietzsche|Queen|Tolkien)$'))
-app.add_handler(CallbackQueryHandler(
-    change_person, pattern='^change_person$'))
-app.add_handler(CallbackQueryHandler(
-    quiz_topic_selected, pattern='^(quiz_prog|quiz_math|quiz_biology)$'))
-app.add_handler(CallbackQueryHandler(quiz_more, pattern='^quiz_more$'))
-app.add_handler(CallbackQueryHandler(
-    change_quiz_topic, pattern='^change_quiz_topic$'))
-app.add_handler(CallbackQueryHandler(default_callback_handler))
+app.add_handler(conv_handler)
 app.run_polling()
